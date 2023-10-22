e96a86562cc7472800e9858862488ab4b6fc770e
unknown <m.sheykhfaraji2001@gmail.com>
2023-10-22 20:02:44 +0330

    delet frontend and create new frontend and install talwindcss

---
+++ frontend/src/core/Home.jsx
@@ -0,0 +1,35 @@
+
+
+function Home() {
+
+
+
+
+    return (
+      <div >
+       <figure class="bg-slate-50 rounded-xl p-8 dark:bg-slate-300">
+     
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia 
+      </div>
+    </figcaption>
+  </div>
+</figure>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import Router from './Routers';
-
+import Router from './Router'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
