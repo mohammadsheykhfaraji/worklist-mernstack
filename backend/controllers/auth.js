0eb11f60dc6d7f2d8df476c705c82ad50eb5512b
unknown <m.sheykhfaraji2001@gmail.com>
2023-11-30 23:39:33 +0330

    complete backend auth

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,25 +2,62 @@ const User=require('../models/user');
 
 exports.signin=async(req,res)=>{
     try {
+        const{email,password}=req.body;
+        const user= await User.findOne({email:email});
         
+    if(user){
+      const isEqual = await bcrypt.compare(password, user.password);
+      
+      if (isEqual) {
+    
+          const token = jwt.sign(
+              {
+                  user: {
+                      userId: user._id.toString(),
+                      email: user.email,
+                      fullname: user.fullname,
+                      role:user.role,
+                  },
+              },
+              process.env.JWT_SECRET
+          );
+
+          return  res.status(200).json({ token, name:user.fullname});
+      } else {
+          const err ="ادرس ایمیل با کلمه عبور همخوانی ندارد";
+          throw err;
+      }
+    }else if(!user){
+      const err = "آدرس ایمیل یا کلمه عبور اشتباه است";
+      throw err;
+    }
+   
     } catch (error) {
-        
+        return res.status(400).json(error)
     }
    
    
 }
 exports.signup=async(req,res)=>{
    try {
-    const user=req.body;
+      await User.userValidation(req.body);
+      const{fullname,email,password}=req.body;
+      const user=await User.findOne({email:email});
+      if(user){
+         return res.status(400).json({message:"قبلا با این ایمیل ثبت نام شده است لطفا لاگین کنید"});
+       }
+       await User.create({ fullname, email, password});
+   return res.status(200).json({fullname, email, password});
+
    } catch (error) {
-    res.send(error)
+     return res.status(400).json(error);
    }
 }
 
 exports.signout=async(req,res)=>{
    try {
-    
+      return res.status(200).json({message:"شما خارج شدید"})
    } catch (error) {
-    
+      return res.status(400).json(error);
    }
 }
--- backend/models/user.js
+++ backend/models/user.js
@@ -14,11 +14,42 @@ const user=new mongoose.Schema({
     password:{
         type:String,
         required:true,
+        maxlenth:255,
+        minlenth:4,
         trim:true,
     },
+    createdate:{
+        type:Date,
+        default:Date.now,
+    },
+    role:{
+        type:Number,
+        default:0
+    }
 
 
 });
 
+userschema.statics.userValidation = function (body) {
+    return schema.validate(body, { abortEarly: false });
+};
+
+userschema.pre("save",function(next){
+    let user=this;
+
+    if(user.isModified("password")){
+        bycrpt.hash(user.password,10,(err,hash)=>{
+            if(err){
+                return next(err);
+            }
+            user.password=hash;
+            next();
+
+        });
+    }else{
+        return next();
+    }
+
+});
 
 module.exports=mongoose.model('user',user);
